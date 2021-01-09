ef80c2b11fdfc4bfa972151620d1f05d146a6171
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-01-09 18:42:31 -0300

    Add routes and StepsHeaders

--- frontend/front-web/src/App.tsx
+++ frontend/front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import NavBar from './NavBar';
-import Home from './Home';
+import Routes from './Routes';
 
 
 function App() {
   return (
-    <>
-      <NavBar/> 
-      <Home/>
-    </>
+    <Routes/>
   );
 }
 
---
+++ frontend/front-web/src/Orders/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  "./styles.css";
+import{ ReactComponent as Logo  } from './logo.svg';
+import StepHeaders from "../StepHeaders";
+
+function Orders () {
+    return (
+        <div className="orders-container">
+            <StepHeaders />
+       
+        </div>
+    )
+}
+
+export default Orders;
